40228546a2744590d19012e800bc36adde5039ea
meeschasch <88081797+meeschasch@users.noreply.github.com>
2022-10-06 23:06:09 +0200

    added some plotting funcs

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ Created on Sun Oct  2 20:27:47 2022
 
 import myres as mr, numpy as np, matplotlib.pyplot as plt
 
-# =============================================================================
-# Testing casing design
-# =============================================================================
+#=============================================================================
+#Testing casing design
+#=============================================================================
 # ids = [0.2, 0.3, 0.5]
 ls = [1000, 1000, 1000]
 # cd = mr.casing_design(ls = ls, ids = ids)
@@ -18,25 +18,30 @@ ls = [1000, 1000, 1000]
 ods = [7.625, 9.625, 10.75]
 wgs = [41, 36, 79.2]
 ids = [0.3, 0.4, 0.5]
-cd = mr.casing_design(ls = ls, ids = ids, ods = ods, wgs = wgs)
+cd = mr.CasingDesign(ls = ls, ids = ids, ods = ods, wgs = wgs)
+print(cd)
+cd.adjust_to_zref(1500, 'down')
+print(cd)
+
+fig, ax = plt.subplots()
 
 #cd.remove_section(2)
 
 #cd.add_section(2, l = 400, id = 9.4)
 #print(cd.ids)
 
-# =============================================================================
-# test welltops
-# =============================================================================
-mywell = mr.well(name = 'GIS1', casing_design = cd, p_res = 250, T_res = 140, S = 2000, welltype = 'inj')
-#mywell.add_welltop('Top Malm')
+# # =============================================================================
+# # test welltops
+# # =============================================================================
+# mywell = mr.well(name = 'GIS1', casing_design = cd, p_res = 250, T_res = 140, S = 2000, welltype = 'inj')
+# #mywell.add_welltop('Top Malm')
 
-print(mywell.find_containing_section(1100))
-# =============================================================================
-# test friction losses
-# =============================================================================
-q = np.arange(1100)
-fl = mywell.get_fl_at_q(q = q)
+# print(mywell.find_containing_section(1100))
+# # =============================================================================
+# # test friction losses
+# # =============================================================================
+# q = np.arange(1100)
+# fl = mywell.get_fl_at_q(q = q)
 
 #plt.plot(q, fl)
 
@@ -55,19 +60,27 @@ fl = mywell.get_fl_at_q(q = q)
 #     well.add_welltop('top reservoir', 2000)
 #     well.add_welltop('surface', 0)
 
-
-# class test():
+# from abc import ABC, abstractmethod
+# class test(ABC):
 #     def __init__(self, attr):
 #         self.attr = attr
-        
-#     @property
-#     def attr(self):
-#         print('Im in the getter')
-#         return self._attr
     
-#     @attr.setter
-#     def attr(self, new_attr):
-#         print('Im in the setter')
-#         self._attr = new_attr
+#     def print_not_abstract(self):
+#         print('printed from the abc')
         
-# t = test('sdf')
+#     @abstractmethod
+#     def print_abstract(self):
+#         pass
+    
+# class subtest(test):
+#     def __init(self, attr):
+#         super().__init__(attr)
+#         self.attr = attr
+        
+#     def print_abstract(self):
+#         print('printed from the subclass')
+
+
+# t = subtest('sdf')
+# t.print_not_abstract()
+# t.print_abstract()
